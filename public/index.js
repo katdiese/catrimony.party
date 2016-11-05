576ed9c3927a3fd5f8dc17ea7835896b09019fcf
Kathy Dieser <katdiese@gmail.com>
2016-11-05 08:40:42 -0600

    playing around with module

--- public/index.js
+++ public/index.js
@@ -1,11 +1,20 @@
-import Header from './src/header';
-
 var HomeListing = React.createClass({
   render() {
     return(
       <div>
         <Header />
         <h1>Meow</h1>
+      </div>
+
+      );
+  }
+});
+
+var Header = React.createClass({
+  render() {
+    return(
+      <div>
+        <h1>Meow!!!!!</h1>
       </div>
 
       );
