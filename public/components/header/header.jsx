e552f7f190b084094f76612c90137c1ebf29223a
Kathy Dieser <katdiese@gmail.com>
2016-11-06 12:42:59 -0700

    reorganized header component

---
+++ public/components/header/header.jsx
@@ -0,0 +1,21 @@
+var React = require('react');
+var CountDown = require('./countDown');
+
+var Header = React.createClass({
+  render: function() {
+    return(
+      <div className="hero">
+        <div className="hero-text">
+          <h1>Holy Catrimony!</h1>
+          <h4>Steve and Kathy are getting married</h4>
+          <h4>December 17, 2017</h4>
+          <h4>Keystone, Colorado</h4>
+        </div>
+        <CountDown />
+      </div>
+
+      );
+  }
+});
+
+module.exports = Header;
