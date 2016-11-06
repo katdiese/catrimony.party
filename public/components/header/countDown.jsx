2af1023a8922fb9dad6b2ed1ef7e0b81a815759c
Kathy Dieser <katdiese@gmail.com>
2016-11-06 13:49:47 -0700

    set up and header

--- public/components/header/countDown.jsx
+++ public/components/header/countDown.jsx
@@ -1,12 +1,14 @@
+var countdown = require('react-count-down');
 var React = require('react');
 
 var CountDown = React.createClass({
-  render: function() {
-    return(
+  render() {
+    var OPTIONS = { endDate: '12/17/2017 3:30 PM', prefix: 'till Catrimony!' }
+    return (
       <div>
+        <countdown options={OPTIONS} />
       </div>
-
-      );
+    );
   }
 });
 
--- public/components/header/header.jsx
+++ public/components/header/header.jsx
@@ -2,7 +2,7 @@ var React = require('react');
 var CountDown = require('./countDown');
 
 var Header = React.createClass({
-  render: function() {
+  render() {
     return(
       <div className="hero">
         <div className="hero-text">
@@ -10,8 +10,8 @@ var Header = React.createClass({
           <h4>Steve and Kathy are getting married</h4>
           <h4>December 17, 2017</h4>
           <h4>Keystone, Colorado</h4>
+          <CountDown />
         </div>
-        <CountDown />
       </div>
 
       );
