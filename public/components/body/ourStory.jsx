c10b702073593df01ca40466f34435a2b392e91f
Kathy Dieser <katdiese@gmail.com>
2016-12-05 20:28:44 -0700

    added title, re built

--- public/components/body/ourStory.jsx
+++ public/components/body/ourStory.jsx
@@ -4,6 +4,7 @@ var OurStory = React.createClass({
   render() {
     return (
       <div className="our-story">
+        <h1 className="text-center calligraphy">Our Story</h1>
         <iframe src='https://cdn.knightlab.com/libs/timeline3/latest/embed/index.html?source=1gdTxKs4HIDAjU1WfgLSW5pT0Jwd1nxPz53i2S0o2ELo&font=Default&lang=en&initial_zoom=2&height=650' width='100%' height='650' frameborder='0'></iframe>
       </div>
 
