38ccb7231c45c63b13cf0b7ea2041c801b011fcd
Kathy Dieser <katdiese@gmail.com>
2017-01-12 21:09:51 -0700

    created countdown timer for site

--- public/components/header/countDown.jsx
+++ public/components/header/countDown.jsx
@@ -1,12 +1,45 @@
-var countdown = require('react-count-down');
 var React = require('react');
 
 var CountDown = React.createClass({
+  getInitialState: function() {
+    var timeRemaining = Date.parse('2017-12-17') - Date.parse(new Date());
+    var sec = Math.floor((timeRemaining/1000) % 60 );
+    var min = Math.floor((timeRemaining/1000/60) % 60);
+    var h = Math.floor( timeRemaining/(1000*60*60) % 24 );
+    var d = Math.floor( timeRemaining / ( 1000*60*60*24 ));
+    return {
+      time: timeRemaining,
+      days: d,
+      hours: h,
+      minutes: min,
+      seconds: sec
+    }
+  },
+  countdownTimer: function(endtime){
+    var timeRemaining = Date.parse(endtime) - Date.parse(new Date());
+    var sec = Math.floor((timeRemaining/1000) % 60 );
+    var min = Math.floor((timeRemaining/1000/60) % 60);
+    var h = Math.floor( timeRemaining/(1000*60*60) % 24 );
+    var d = Math.floor( timeRemaining / ( 1000*60*60*24 ));
+    this.setState({
+      seconds: sec,
+      minutes: min,
+      hours: h,
+      days:  d
+    })
+  },
   render() {
-    var OPTIONS = { endDate: '12/17/2017 3:30 PM', prefix: 'till Catrimony!' }
+    var self = this;
+    if(this.state.time >= 0) {
+    setInterval(function() {
+      self.countdownTimer('2017-12-17')
+    }, 1000)
+    }
     return (
       <div>
-        <countdown options={OPTIONS} />
+        <h1>There are</h1>
+        <h3>{ this.state.days } days, { this.state.hours } hours, { this.state.minutes } minutes, and { this.state.seconds } seconds</h3>
+        <h1>Till the day of our wedding!!!</h1>
       </div>
     );
   }
