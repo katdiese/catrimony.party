b2ed48f2c44d1590c5d3d12ee15e7b0df02452d2
Kathy Dieser <katdiese@gmail.com>
2017-01-30 19:49:16 -0700

    working on nav functionality

--- public/components/header/nav.jsx
+++ public/components/header/nav.jsx
@@ -3,11 +3,13 @@ var React = require('react');
 var Nav = React.createClass({
   showClose: function() {
     document.getElementById("close").style.display = "block";
+    document.getElementById("mobile-nav").style.display = "block";
     document.getElementById("hamburger").style.display = "none";
   },
   showHamburger: function() {
     document.getElementById("hamburger").style.display = "block";
     document.getElementById("close").style.display = "none";
+    document.getElementById("mobile-nav").style.display = "none";
   },
   render() {
     return(
@@ -15,15 +17,18 @@ var Nav = React.createClass({
         <nav className="nav">
         <img className="hamburger" id="hamburger" onClick={() => this.showClose()} src="/images/hamburger.png"/>
         <img className="close" id="close" onClick={() => this.showHamburger()} src="/images/close.png" />
-          <i className="fa fa-tree" aria-hidden="true"></i>
-          <a className="desk-nav" href="#weekend-deets">
-            Details
-          </a>
+          <a className="desk-nav" href="#weekend-deets">Details</a>
           <a className="desk-nav" href="#getting-here">Getting Here</a>
           <a className="desk-nav" href="#story">Story</a>
           <a className="desk-nav" href="#gallery">Gallery</a>
-          <a className="desk-nav" href="#vip">VIP</a>
           <a className="desk-nav" target="_blank" href="https://www.newlywish.com/registry/catrimony">Registry</a>
+          <ul className="mobile-nav" id="mobile-nav">
+            <li onClick={() => this.showHamburger()}><a href="#weekend-deets">Details</a></li>
+            <li onClick={() => this.showHamburger()}><a href="#getting-here">Getting Here</a></li>
+            <li onClick={() => this.showHamburger()}><a href="#story">Story</a></li>
+            <li onClick={() => this.showHamburger()}><a href="#gallery">Gallery</a></li>
+            <li onClick={() => this.showHamburger()}><a target="_blank" href="https://www.newlywish.com/registry/catrimony">Registry</a></li>
+          </ul>
         </nav>
       </div>
 
