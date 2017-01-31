4df08c7ef8bf9e284c8300feaf7862c688eedc30
Kathy Dieser <katdiese@gmail.com>
2017-01-30 19:49:02 -0700

    content

--- public/components/header/header.jsx
+++ public/components/header/header.jsx
@@ -9,10 +9,11 @@ var Header = React.createClass({
         <Nav />
         <div className="hero">
           <div className="hero-text">
-            <h1>Holy Catrimony!</h1>
-            <h4>Steve and Kathy are getting married</h4>
-            <h4>December 17, 2017</h4>
-            <h4>Keystone, Colorado</h4>
+            <h1 className="calligraphy">Holy Catrimony!</h1>
+            <h4 className="calligraphy" >Steve and Kathy are getting married</h4>
+            <h5 className="short-lines">December 17, 2017</h5>
+            <h5 className="short-lines">Keystone, Colorado</h5>
+            <h6 className="short-lines">Attire: Black Tie</h6>
             <h1 className="accents"> &#40; &#34; &#41; </h1>
           </div>
         </div>
