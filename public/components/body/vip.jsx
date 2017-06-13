139ed193bb61cecac2dd85f61f23f0e410edb2db
Kathy Dieser <katdiese@gmail.com>
2017-06-12 21:42:48 -0600

    steve updated content

--- public/components/body/vip.jsx
+++ public/components/body/vip.jsx
@@ -15,9 +15,9 @@ var Vip = React.createClass({
                 <img className="party-img" src="/images/party/maids/Kim.png" />
               </div>
               <div className="eight columns">
-                <h4>Maid of Honor</h4>
+                <h4>Matron of Honor</h4>
                 <p>
-                  <strong>Kim Dieser</strong> has been Kathy’s big sister for literally as long as she can remember. She is the most loyal, reliable, thoughtful, and helpful person Kathy has ever met, and one of her personal heroes.
+                  <strong>Kim</strong> has been Kathy’s big sister for literally longer than she can remember. She is the most loyal, reliable, thoughtful, and helpful person Kathy has ever met, and one of her personal heroes.
                 </p>
               </div>
             </div>
@@ -26,9 +26,9 @@ var Vip = React.createClass({
                 <img className="party-img" src="/images/party/maids/katie.jpg" />
               </div>
               <div className="eight columns">
-                <h4>Maid of Honor</h4>
+                <h4>Matron of Honor</h4>
                 <p>
-                  <strong>Katie Kissel</strong> is Kathy’s best friend from college, and has been one of the most important people in Kathy’s life. Katie is Kathy’s girl soul mate and has always been there when needed most. Their relationship has grown stronger despite the distance, not having lived in the same state since college.
+                  <strong>Katie</strong> and Kathy have known each other since college, and she has been one of the most important people in Kathy’s life. Katie is Kathy’s girl soul mate and has always been there when needed most.
                 </p>
               </div>
             </div>
@@ -39,7 +39,7 @@ var Vip = React.createClass({
               <div className="eight columns">
                 <h4>Bridesmaid</h4>
                 <p>
-                  <strong>Danielle Hall</strong> has been Kathy’s older sister a slightly shorter amount of time than Kim, but they have been close friends since high school. Dani is one of the funniest, most creative people Kathy has ever met and it has always inspired her to follow her own creative passions.
+                  <strong>Danielle</strong> has been Kathy’s older sister a slightly shorter amount of time than Kim, but they have been close friends since high school.
                 </p>
               </div>
             </div>
@@ -50,7 +50,7 @@ var Vip = React.createClass({
               <div className="eight columns">
                 <h4>Bridesmaid</h4>
                 <p>
-                  <strong>Kristin Straily</strong> is one of Kathy’s best friends in Colorado. The two met through Steve’s job (they still work together) and instantly bonded on how bad Steve can be with girls. Kristin is the only person that Kathy’s ever met that plays underwater hockey (until she met some of Kristin’s teammates, of course).
+                  <strong>Kristin</strong> is one of Kathy’s best friends in Colorado. Kathy met Kristin through Steve’s job (they still work together) and instantly bonded on how bad Steve can be with girls.
                 </p>
               </div>
             </div>
@@ -61,7 +61,7 @@ var Vip = React.createClass({
               <div className="eight columns">
                 <h4>Bridesmaid</h4>
                 <p>
-                  <strong>Elena Korolkova</strong> is another of Kathy’s best friends in Colorado. Kathy likes to think of their friendship as the one good thing that came out of working as a buyer at UNFI. Elena is probably the most hospitable person that Kathy has ever met and one of the warmest friends a person could have.
+                  <strong>Elena</strong> is another of Kathy’s best friends in Colorado. Kathy likes to think of their friendship as the one good thing that came out of working as a buyer at UNFI.
                 </p>
               </div>
             </div>
@@ -74,7 +74,7 @@ var Vip = React.createClass({
               <div className="eight columns">
                 <h4>Best Man</h4>
                 <p>
-                  Brian Colonnese will be the easiest guy at the wedding to start a conversation with. For a good ice breaker, just ask him how he and Steve met. Friends since College.
+                  <strong>Brian</strong> will be the easiest guy at the wedding to start a conversation with. For a good ice breaker, just ask him how he and Steve met. Friends since College.
                 </p>
               </div>
             </div>
@@ -85,7 +85,7 @@ var Vip = React.createClass({
               <div className="eight columns">
                 <h4>Groomsman</h4>
                 <p>
-                  Kelvin Dieser is the only guy to ever match Steve at his favorite board game. Unfortunately for the two of them, that means neither of them has ever won. Future brother in law.
+                  <strong>Kelvin</strong> is the only guy to ever match Steve at his favorite board game. Unfortunately for the two of them, that means neither of them has ever won. Future brother in law.
                 </p>
               </div>
             </div>
@@ -96,7 +96,7 @@ var Vip = React.createClass({
               <div className="eight columns">
                 <h4>Groomsman</h4>
                 <p>
-                  Danielle Hall has been Kathy’s older sister a little shorter amount of time than Kim, but they have been close friends since high school. Dani is one of the funniest, most creative people Kathy has ever met and it has always inspired her to follow her own creative passions.
+                  <strong>Mark</strong> and Steve have spent more hours studying together at UConn than fish spend in the water. Neither graduated with honors.
                 </p>
               </div>
             </div>
@@ -107,7 +107,7 @@ var Vip = React.createClass({
               <div className="eight columns">
                 <h4>Groomsman</h4>
                 <p>
-                  John Longo literally taught Steve everything he knows about snowboarding. To return the favor Steve taught Longo how to build a cabin. Friends since college.
+                  <strong>John</strong> literally taught Steve everything he knows about snowboarding. To return the favor Steve taught Longo how to build a cabin. Friends since college.
                 </p>
               </div>
             </div>
@@ -118,7 +118,7 @@ var Vip = React.createClass({
               <div className="eight columns">
                 <h4>Groomsman</h4>
                 <p>
-                  Elena Korolkova is another of Kathy’s best friends in Colorado. Kathy likes to think of their friendship as the one good thing that came out of working as a buyer at UNFI. Elena is probably the most hospitable person that Kathy has ever met and one of the warmest friends a person could have.
+                  <strong>Kyle</strong> and Steve share all the simple hobbies together. Holidays, breweries, and marvel movies. Kyle moved out to Colorado a year after Steve and Kathy with his girlfriend Jen, but they have been friends since college.
                 </p>
               </div>
             </div>
@@ -129,7 +129,7 @@ var Vip = React.createClass({
             <img src="/images/party/kids.jpg" />
           </div>
           <div className="six columns">
-            <p className="text-center">What's a wedding without a little "awwww!!!" These are Katie's gorgeous kids Gus and Charlie Kissel. Gus will walk as the ring bearer and Charlie (Kathy's goddaughter) will be the flower girl.</p>
+            <p className="text-center">What's a wedding without a little "awwww!!!" These are Katie's gorgeous kids Gus and Charlie. Gus will walk as the ring bearer and Charlie (Kathy's goddaughter) will be the flower girl.</p>
           </div>
         </div>
       </div>
